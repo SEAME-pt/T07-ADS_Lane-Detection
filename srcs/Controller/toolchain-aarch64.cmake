8ce4e1f301129098ceb89c9abaf45b479dcbc74e
hmaciel- <heitormp@protonmail.com>
2025-05-29 15:24:14 +0100

    FEAT: cross compilçation with MPC

--- srcs/Controller/toolchain-aarch64.cmake
+++ srcs/Controller/toolchain-aarch64.cmake
@@ -1,24 +1,23 @@
-# toolchain-aarch64.cmake
-
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
-
-set(CMAKE_C_FLAGS "--sysroot=${CMAKE_SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "--sysroot=${CMAKE_SYSROOT}" CACHE STRING "" FORCE)
-
-
-# Aponta para o sysroot
 set(CMAKE_SYSROOT /opt/jetson-sysroot)
+set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/pkgconfig")
 
-# Cross-compilers
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
-# Ajuda o CMake a usar o sysroot corretamente
+# Flags de compilação (compilador usa o sysroot)
+set(CMAKE_C_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_CXX_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT}")
+
+# Flags de linkagem para garantir uso da libc do sysroot
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -Wl,--dynamic-linker=/lib/ld-linux-aarch64.so.1 -L${CMAKE_SYSROOT}/lib -L${CMAKE_SYSROOT}/usr/lib -L${CMAKE_SYSROOT}/lib/aarch64-linux-gnu -L${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu")
+
+# Caminhos de busca
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-# Forçar a utilização de bibliotecas estáticas
-set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -Wl,--whole-archive -lpthread -Wl,--no-whole-archive")
 
+# Pkg-config (garante que ele ache as libs no sysroot)
+set(ENV{PKG_CONFIG_DIR} "")
+set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
+set(ENV{PKG_CONFIG_SYSROOT_DIR} "${CMAKE_SYSROOT}")
